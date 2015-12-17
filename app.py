6c9e1f9b20d35992f845b6ed1e4357d8f4072388
ChengYuHsieh <danielhsieh01@gmail.com>
2015-12-17 10:58:08 +0800

    static folder configured

--- app.py
+++ app.py
@@ -1,11 +1,14 @@
+import os
 from flask import Flask
 from flask import send_file
 
-app = Flask(__name__, static_folder='front_end/dist', static_url_path='')
+STATIC_FOLDER = os.path.join(os.getcwd(), 'front_end/dist')
+
+app = Flask(__name__, static_folder=STATIC_FOLDER, static_url_path='')
 
 @app.route("/")
 def index():
     return send_file('front_end/dist/index.html')
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host='0.0.0.0')
