60a30179d93b54b8d82a690c3e29f39733fae3fc
ChengYuHsieh <danielhsieh01@gmail.com>
2015-12-17 09:03:17 +0800

    changed static folder dir

--- app.py
+++ app.py
@@ -1,11 +1,11 @@
 from flask import Flask
 from flask import send_file
 
-app = Flask(__name__, static_url_path='')
+app = Flask(__name__, static_folder='front_end/dist', static_url_path='')
 
 @app.route("/")
 def index():
-    return send_file('templates/index.html')
+    return send_file('front_end/dist/index.html')
 
 if __name__ == "__main__":
     app.run(debug=True)
