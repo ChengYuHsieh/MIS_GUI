794322ad34f5d1552d1fdae5070663318d93ddef
ChengYuHsieh <danielhsieh01@gmail.com>
2015-12-22 15:44:22 +0800

    XHR host changed

--- front_end/src/app/line/line.controller.js
+++ front_end/src/app/line/line.controller.js
@@ -7,7 +7,7 @@
 
   /** @ngInject */
   function LineController($http) {
-    $http.get("http://localhost:5000/api/database/linechart").then(function(res){
+    $http.get("/api/database/linechart").then(function(res){
         var data = res.data;
         var chart = c3.generate({
             bindto: '#chart',
